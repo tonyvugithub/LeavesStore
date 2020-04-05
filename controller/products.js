ec4cce70669bc0c8d8da019e2e7df22a2b708970
tonyvugithub <55034244+tonyvugithub@users.noreply.github.com>
2020-04-05 14:59:19 -0400

    abc

--- controller/products.js
+++ controller/products.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const fetch = require("node-fetch");
-const jwt = require('jsonwebtoken');
-const { User } = require("../models/user");
 const router = express.Router();
-
 
 router.get("/", async (req, res) => {
   let categories;
   await fetch(`${process.env.BASE_URL}/api/category`)
-    .then(res => res.json())
-    .then(data => (categories = data))
-    .catch(err => console.log(err));
+    .then((res) => res.json())
+    .then((data) => (categories = data))
+    .catch((err) => console.log(err));
   res.render("products", {
     title: "Products Page",
     categories: categories,
@@ -18,7 +15,10 @@ router.get("/", async (req, res) => {
     topPromote: "Free Shipping on Order more than $50",
     userLoggedIn: req.isAuthenticated(),
     userFirstname: req.isAuthenticated() ? req.user.firstname : "",
-    dashboardLink: req.isAuthenticated() && req.user.isSaleClerk ? '/users/clerk/myaccount' : '/users/myaccount'
+    dashboardLink:
+      req.isAuthenticated() && req.user.isSaleClerk
+        ? "/users/clerk/myaccount"
+        : "/users/myaccount",
   });
 });
 
@@ -27,16 +27,16 @@ router.get("/collection", async (req, res) => {
 
   //Call to categoryAPI to fetch all categories
   await fetch(`${process.env.BASE_URL}/api/category`)
-    .then(res => res.json())
-    .then(data => (categories = data))
-    .catch(err => console.log(err));
+    .then((res) => res.json())
+    .then((data) => (categories = data))
+    .catch((err) => console.log(err));
 
   if (req.query.sortBy == "all") {
     //Call to productAPI to fetch data of ALL product
     await fetch(`${process.env.BASE_URL}/api/product`)
-      .then(res => res.json())
-      .then(data => (products = data))
-      .catch(err => console.log(err));
+      .then((res) => res.json())
+      .then((data) => (products = data))
+      .catch((err) => console.log(err));
 
     res.render("products", {
       title: "Products Page",
@@ -44,14 +44,18 @@ router.get("/collection", async (req, res) => {
       productList: products,
       products_active: true,
       categoryTitle: "full",
-      topPromote: "Buy One, Get One Free"
+      topPromote: "Buy One, Get One Free",
+      userLoggedIn: req.isAuthenticated(),
+      userFirstname: req.isAuthenticated() ? req.user.firstname : "",
+      dashboardLink:
+        req.isAuthenticated() && req.user.isSaleClerk ? "/users/clerk/myaccount" : "/users/myaccount",
     });
   } else {
     //Call to productAPI to fetch data of provided CATEGORY
     await fetch(`${process.env.BASE_URL}/api/product/${req.query.sortBy}`)
-      .then(res => res.json())
-      .then(data => (products = data))
-      .catch(err => console.log(err));
+      .then((res) => res.json())
+      .then((data) => (products = data))
+      .catch((err) => console.log(err));
 
     res.render("products", {
       title: "Products Page",
@@ -59,7 +63,11 @@ router.get("/collection", async (req, res) => {
       productList: products,
       products_active: true,
       categoryTitle: req.params.sortBy,
-      topPromote: "Buy One, Get One Free"
+      topPromote: "Buy One, Get One Free",
+      userLoggedIn: req.isAuthenticated(),
+      userFirstname: req.isAuthenticated() ? req.user.firstname : "",
+      dashboardLink:
+        req.isAuthenticated() && req.user.isSaleClerk ? "/users/clerk/myaccount" : "/users/myaccount",
     });
   }
 });
